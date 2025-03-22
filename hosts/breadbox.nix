a961f4fc09943a774de8a1108ed1b6e7116b5e58
Nathan Park <nalhan.park@gmail.com>
2025-03-22 15:20:25 -0700

    new idea sound

--- hosts/breadbox.nix
+++ hosts/breadbox.nix
@@ -95,6 +95,14 @@
   #  support32Bit = true;
   #};
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
   ############
   # PROGRAMS #
   ############
