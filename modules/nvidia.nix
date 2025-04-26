43b39a03af4dc2efd4290ca3ea7a5493e427efc6
Nathan Park <nalhan.park@gmail.com>
2025-04-25 22:31:23 -0700

    update flake.lock -- set nvidia drivers to latest

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -17,14 +17,19 @@
     open = true;
 
     nvidiaSettings = true;
+
+    # TODO: this should probably be set by the host's config file...
     
-    # we're using the beta version here. could override this in the host's config 
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.86.16";
-      sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-      openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-      settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
-      usePersistenced = false;
-    };
+    # 4/25/25 - changed to latest, 4080 super seems to be stable now
+    package = config.boot.kernelPackages.nvidiaPackages.latest; 
+
+    
+    #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #  version = "570.86.16";
+    #  sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+    #  openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
+    #  settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
+    #  usePersistenced = false;
+    #};
   };
 }
